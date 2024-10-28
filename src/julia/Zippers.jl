3e87437d7cc89f26c6ce8d609e304fac4efc143e
Thomas Dubos <dubos@llmd.ipsl.fr>
2024-10-28 14:05:59 +0100

    Add Voronoi mesh info needed for transport

---
+++ src/julia/Zippers.jl
@@ -0,0 +1,23 @@
+module Zippers
+
+using Base.Broadcast: Broadcast, Broadcasted, combine_eltypes
+
+struct Zipper end
+const zipper = Zipper()
+
+@inline function Broadcast.materialize!(::Zipper, bc::Broadcasted)
+    T = combine_eltypes(bc.f, bc.args)
+    return materialize_zipper(Tuple(T.types), bc)
+end
+
+@inline function materialize_zipper(types, bc)
+    x = map(E -> similar(bc, E), types)
+    @inbounds for i in eachindex(bc)
+        foreach(x, bc[i]) do xx, yy
+            xx[i] = yy
+        end
+    end
+    return x
+end
+
+end
